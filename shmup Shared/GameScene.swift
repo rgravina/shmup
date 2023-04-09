45ef3e5c3bf7c4036071cf0054cd44580637374b
Robert Gravina <robert.gravina@gmail.com>
2023-04-09 16:33:49 +0900

    Wrap ship around edges

--- shmup Shared/GameScene.swift
+++ shmup Shared/GameScene.swift
@@ -11,6 +11,11 @@ enum KeyCodes: UInt16 {
     case up = 126
 }
 
+struct Coordinate {
+    var x: Int
+    var y: Int
+}
+
 class GameScene: SKScene {
     private let movementSpeed = 8.0
     private let spriteScale = 8.0
@@ -35,6 +40,7 @@ class GameScene: SKScene {
 
     private func displaySprite(imageNamed: String) -> SKSpriteNode  {
         let sprite = SKSpriteNode(imageNamed: imageNamed)
+        sprite.anchorPoint = CGPoint(x: 0, y: 1.0)
         sprite.setScale(spriteScale)
         sprite.texture?.filteringMode = .nearest
         addChild(sprite)
@@ -70,6 +76,20 @@ class GameScene: SKScene {
         case .none:
             break
         }
+
+        let cooordinate = coords(position: ship.position)
+        if (cooordinate.x < 0) {
+            ship.position = position(coordinate: Coordinate(x: 120, y: cooordinate.y))
+        }
+        if (cooordinate.x > 120) {
+            ship.position = position(coordinate: Coordinate(x: 0, y: cooordinate.y))
+        }
+        if (cooordinate.y < 0) {
+            ship.position = position(coordinate: Coordinate(x: cooordinate.x, y: 120))
+        }
+        if (cooordinate.y > 120) {
+            ship.position = position(coordinate: Coordinate(x: cooordinate.x, y: 0))
+        }
     }
 
 
@@ -96,5 +116,16 @@ class GameScene: SKScene {
         default:
             return .none
         }
+    }
+
+    private func coords(position: CGPoint) -> Coordinate {
+        return Coordinate(x: Int(position.x/speed) + 64, y: Int(position.y * -1/speed) + 64)
+    }
+
+    private func position(coordinate: Coordinate) -> CGPoint {
+        return CGPoint(
+            x: (CGFloat(coordinate.x) - 64) * speed,
+            y: (CGFloat(coordinate.y * -1) + 64) * speed
+        )
     }
 }
